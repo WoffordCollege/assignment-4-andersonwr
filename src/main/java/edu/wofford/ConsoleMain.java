25cde3a76d588550155d6c72f0a346c1da67b0ef
andersonwr <wranderson18@gmail.com>
2018-10-07 00:08:47 -0400

    Implemented ConsoleMain.java

--- src/main/java/edu/wofford/ConsoleMain.java
+++ src/main/java/edu/wofford/ConsoleMain.java
@@ -6,6 +6,21 @@ public class ConsoleMain {
 
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
+		TicTacToeModel game = new TicTacToeModel();
+		TicTacToeModel.Result result = TicTacToeModel.Result.NONE;
 
+		while (result == TicTacToeModel.Result.NONE){
+			game.setMarkAt(keyboard.nextInt(), keyboard.nextInt());
+			System.out.println(game.toString());
+			result = game.getResult();
+		}
+
+		if(result == TicTacToeModel.Result.XWIN){
+			System.out.println("X wins");
+		} else if (result == TicTacToeModel.Result.OWIN){
+			System.out.println("O wins");
+		} else {
+			System.out.println("Tie");
+		}
 	}
 }
